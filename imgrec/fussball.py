73f7be123e0550887eec5ca5b9d1427ffae00ed9
Paul Brook <paul@nowt.org>
2013-06-25 06:12:53 +0100

    Migrate to new API
    Migrate to opencv 2.x API
    
    Signed-off-by: Paul Brook <paul@nowt.org>

--- imgrec/fussball.py
+++ imgrec/fussball.py
@@ -1,4 +1,8 @@
-import cv
+#! /usr/bin/env python
+
+import numpy as np
+import cv2
+import time
 #import sys
 
 
@@ -32,56 +36,48 @@ class fussballcv():
 		self.interactive = interactive
 
 		if live:
-			self.cap = cv.CreateCameraCapture(0)
+			self.cap = cv2.VideoCapture(0)
 		else:
 			filename = 'out.avi'
-			self.cap = cv.CreateFileCapture(filename)
+                        self.cap = cv2.VideoCapture(filename)
 			if not self.cap  :
 				raise "file load fail!"
 			else:
-				print "Frame: " + str(cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_POS_FRAMES ))
-				print "Frame Height: " + str(cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_FRAME_HEIGHT  ))
-				print "FPS: " + str(cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_FPS  ))
-				print "FourCC: " + str(cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_FOURCC ))
-			frame = cv.QueryFrame(self.cap)
+				print "Frame: " + str(self.cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES ))
+				print "Frame Height: " + str(self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT  ))
+				print "FPS: " + str(self.cap.get(cv2.cv.CV_CAP_PROP_FPS  ))
+				print "FourCC: " + str(self.cap.get(cv2.cv.CV_CAP_PROP_FOURCC ))
+                        (retval, frame) = self.cap.read()
 
-		self.playmask = cv.LoadImage("Table.png")
-		self.polemask = cv.LoadImage("Poles.png")
-		self.linemask = cv.LoadImage("Lines.png")
-
-		# create some buffers
-		self.yuvframe = cv.CreateMat(frame.height, frame.width, cv.CV_8UC3)
-		self.imgThreshed = cv.CreateMat(frame.height, frame.width, cv.CV_8UC1)
-		self.imgDilated =  cv.CreateMat(frame.height, frame.width, cv.CV_8UC1)
-		self.maskedFrame = cv.CreateMat(frame.height, frame.width, cv.CV_8UC3)
-		self.storage = cv.CreateMemStorage(0)
-
+		self.playmask = cv2.imread("Table.png")
+		self.polemask = cv2.imread("Poles.png")
+		self.linemask = cv2.imread("Lines.png")
+                self.all_mask = self.playmask & self.polemask & self.linemask
 
 	def init_interactive(self):
-		cv.NamedWindow('Table', cv.CV_WINDOW_AUTOSIZE)
+		cv2.NamedWindow('Table', cv.CV_WINDOW_AUTOSIZE)
 		#cv.NamedWindow('h', cv.CV_WINDOW_AUTOSIZE)
-		cv.NamedWindow('s', cv.CV_WINDOW_AUTOSIZE)
+		cv2.NamedWindow('s', cv.CV_WINDOW_AUTOSIZE)
 		#cv.NamedWindow('l', cv.CV_WINDOW_AUTOSIZE)
-
-	def __del__(self):
-		cv.DestroyAllWindows()
 
 	def update(self, ball ):
 		key = -1
-		key = cv.WaitKey(5) # get user input
+		key = cv2.waitKey(1) # get user input
 
 
-		frame = cv.QueryFrame(self.cap)
+                t1 = time.time()
+                (retval, frame) = self.cap.read()
 		if frame != None:
 			bigblob = self.locateBlobs(frame)
 
 				#minBallDiameter = 5
-			CV_RED = cv.Scalar(0,0,255)
+			CV_RED = (0,0,255)
 			if bigblob != None:
-				cv.Circle(frame, (int(bigblob[1][0]), int(bigblob[1][1])), int(bigblob[2]), CV_RED)
+				cv2.circle(frame, (int(bigblob[0][0]), int(bigblob[0][1])), int(bigblob[1]), CV_RED)
 
 			#if key == ord('s'):
 			#	saveFrame(frame)
+                t2 = time.time()
 
 		if key == ord('q') and self.interactive : # 'q'
 			return -1
@@ -89,64 +85,56 @@ class fussballcv():
 			cv.SetCaptureProperty(self.cap, cv.CV_CAP_PROP_POS_FRAMES, cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_POS_FRAMES) + 400)
 		if key == ord('k') and not self.live and self.interactive :
 			cv.SetCaptureProperty(self.cap, cv.CV_CAP_PROP_POS_FRAMES, cv.GetCaptureProperty(self.cap, cv.CV_CAP_PROP_POS_FRAMES) - 400)
-		if self.interactive:
-			cv.ShowImage('Table', frame) # show the image
+                if self.interactive:
+			cv2.imshow('Table', frame) # show the image
+                print 1/(t2 - t1)
 		return 0
 
 
 	def locateBlobs(self, frame):
 
 		#load mask
-		cv.And(frame, self.playmask, self.maskedFrame ) # mask off play area
-		cv.And(self.maskedFrame, self.polemask, self.maskedFrame ) # mask off play area
-		cv.And(self.maskedFrame, self.linemask, self.maskedFrame ) # mask off play area
+                maskedFrame = frame & self.all_mask;
 
 		#gaussian blur
-		cv.Smooth(self.maskedFrame,self.maskedFrame, cv.CV_GAUSSIAN,3,3)
+                cv2.GaussianBlur(maskedFrame, (3, 3), 0, maskedFrame)
 
 
-
-		#imgContour =  cv.CreateMat(frame.height, frame.width, cv.CV_8UC1) # scratch space for findcontours
-
-
-		#convert to HLS colour space
-		cv.CvtColor(self.maskedFrame, self.yuvframe, cv.CV_RGB2HLS)
+		#convert to HLS colour space (slow, not currently used)
+		#yuvframe = cv2.cvtColor(maskedFrame, cv2.COLOR_RGB2HLS)
 		# threshold on saturation being high
-		cv.InRangeS(self.maskedFrame, cv.Scalar(0, 0, 200), cv.Scalar(255, 255, 255), self.imgThreshed)
+                low = np.array([200, 200, 200], np.uint8)
+                high = np.array([255, 255, 255], np.uint8)
+		threshframe = cv2.inRange(maskedFrame, low, high)
 
 
 		#dilate the Thresholded image
-		cv.Dilate(self.imgThreshed, self.imgDilated)
+                cv2.dilate(threshframe, None, dst=threshframe)
+                #self.t = threshframe.copy()
 
-		# copy dialated int
-		#cv.Copy(imgDilated, imgContour)
-		contours = cv.FindContours(self.imgDilated,  self.storage)
+		contours, hierarchy = cv2.findContours(threshframe, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 
 
 		maxContour = None
 		maxContourArea = 0
 
-		#contours.h_next()
-		try:
-			while contours != None:
-				# get points
-				circle = cv.MinEnclosingCircle(contours)
-				if maxContourArea < cv.ContourArea(contours):
-					maxContour = contours
-					maxContourArea =  cv.ContourArea(contours)
-				contours = contours.h_next()
-			circle = cv.MinEnclosingCircle(maxContour)
-			return circle
-		except:
-			print 'meh'
-			return None
-
-
+                for c in contours:
+                        # get points
+                        circle = cv2.minEnclosingCircle(c)
+                        area = cv2.contourArea(c)
+                        if maxContourArea < area:
+                                maxContour = c
+                                maxContourArea = area
+                if maxContour is None:
+                    return None
+                circle = cv2.minEnclosingCircle(maxContour)
 		# just return the biggest circle
+                return circle
 
 	def saveFrame(self, frame):
-		cv.SaveImage("template.png", frame)
+		cv.imsave("template.png", frame)
+
 
 
 if __name__ == "__main__":
